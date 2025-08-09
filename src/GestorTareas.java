38d64c8795b39c10dfba03f65dfb9a688942d3e5
AllanKill95 <allan.sanchez3095@unitec.edu>
2025-08-08 20:10:20 -0600

    //Generamos el Array y las clases en esta parte codigo//

---
+++ src/GestorTareas.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestionamientoTareas {
+
+
+    static class Tarea {
+        private String descripcion;
+        private String fechaLimite;
+        private String prioridad;
+        private String categoria;
+        private boolean completada;
+//Generamos el Array y las clases en esta parte del codigo//
